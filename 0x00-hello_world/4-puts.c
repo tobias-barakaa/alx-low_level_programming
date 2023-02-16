13e4a36e777832a1cd9a09b8a4a7fe6d50096eaa
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-16 15:08:31 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,13 @@
 /**
  *main - Entry point
  *
- *Return: Always 0 (Success)
+ *Return Always 0 (Success)
  */
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
-       	return (0);
+       	
+	return (0);
 }
