a93195fa3824c8cc60a30ffc5b7f99fc7105d7e6
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-16 15:22:16 -0800

    char

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ *  main - Entry point
  *
- * Return: Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-       	printf("with proper grammar, but the outcome is a piece of art,\n")
-       
-     	return (0);
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	
+	return (0);
 }
