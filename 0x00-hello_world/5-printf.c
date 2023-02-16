5023523203ed9ae6a2000ce65781181c001d10f0
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-16 15:30:09 -0800

    comm

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,7 @@
  */
 int main()
 {
-   
-    printf("With proper grammar, but the outcome is a piece of art,\n");
-
-    return 0;
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	
+	return (0);
 }
