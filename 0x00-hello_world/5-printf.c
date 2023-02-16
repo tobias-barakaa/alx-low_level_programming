586cd4315f41768418b245b131cf52a8fdaa33ad
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-16 15:43:38 -0800

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,8 @@
  *
  *  Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-	printf("With proper grammar, but the outcome is a piece of art,\n");
-	
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+        return (0);
 }
