a94e55d4c5319f2b7cc3372f779e043f8ec32155
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-16 14:55:42 -0800

    com

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,6 +7,5 @@
 int main(void)
 {
  puts("\"Programming is like building a multilingual puzzle");
-
- return (0);
+    return (0);
 }
