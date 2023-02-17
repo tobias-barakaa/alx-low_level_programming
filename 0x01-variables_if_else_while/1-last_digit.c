6bed5aea5af81d1f3c5f5b751c9078ebca3ebe7e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 12:47:45 -0800

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("%d and is greater than 5\n", n);
 else if(n == 0){
 printf("%d and is 0\n", n);
 }
-else if(n < 6 || != 0){
+else if(n < 6 && != 0){
 printf("%d and is less than 6 and not 0\n", n);
 }
 }
