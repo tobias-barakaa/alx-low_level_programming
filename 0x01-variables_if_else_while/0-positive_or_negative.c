176c3d36ebf24694c13076e32d396c8d83dd0a3c
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 12:19:38 -0800

    entry line

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - prints the alphabet in lowercase, and then in uppercase,
  * followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if(n > 0) {
-		printf("%d is positive \n", n)
+	        printf("%d is positive \n", n)
 	}
-	else if(n == 0){
-		printf("%d is zero \n", n)
+	else if(n == 0)
+	{
+              printf("%d is zero \n", n)
 	}
-	else if(n < 0){
-		printf("%d is negative \n", n)
+        else if(n < 0){
+            printf("%d is negative \n", n)
 	}
 	return (0);
 }
