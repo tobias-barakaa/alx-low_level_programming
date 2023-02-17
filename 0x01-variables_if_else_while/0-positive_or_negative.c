23e784e47937d38cc5bac57c7ceef3182cbee051
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 11:57:53 -0800

    gicom

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,17 +6,18 @@
  * it executes, and prints it
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 
 {
+
 	int n;
 	srand(time(0));
         n = rand() - RAND_MAX / 2;
-        if (n > 0)
+        if(n > 0)
 		printf("%d is positive\n", n);
-        else if (n == 0)
+        else if(n == 0)
 		printf("%d is zero\n", n);
-        else if (n < 0)
+        else if(n < 0)
 		printf("%d is negative\n", n);
-	return (0);
+	return(0);
 }
