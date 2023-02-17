5aadccb54fe012b6878152a631c7b7ff6c81cd4e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 15:57:08 -0800

    getchar

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,12 +8,11 @@
  */
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i < 10; i++) {
-        putchar(i + '0');
-    }
-
-    putchar('\n');
-    return 0;
+for (i = 0; i < 10; i++) {
+putchar(i + '0');
+}
+putchar('\n');
+return 0;
 }
