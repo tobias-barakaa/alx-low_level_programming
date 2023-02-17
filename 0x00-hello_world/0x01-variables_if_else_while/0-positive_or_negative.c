27253bb2600d67b2a0d58508705b5563afd76a7d
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 10:02:11 -0800

    git-push

--- 0x00-hello_world/0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x00-hello_world/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,7 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-        srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	int n = rand() - RAND_MAX / 2;
 
 	if(n > 0) {
         printf("%d is positive\n" n);
