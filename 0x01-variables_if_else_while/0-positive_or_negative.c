d87de14df6768a42b78be818ae35b2cde1972fa7
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 12:28:34 -0800

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,15 +9,15 @@
  */
 int main(void)
 {
-        int n;
+int n;
 
-	srand(time(0));
-        n = rand() - RAND_MAX / 2;
-	if (n > 0)
-                printf("%d is positive\n", n);
-        else if (n == 0)
-	        printf("%d is zero\n", n);
-        else if (n < 0)
-	        printf("%d is negative\n", n);
-        return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+return (0);
 }
