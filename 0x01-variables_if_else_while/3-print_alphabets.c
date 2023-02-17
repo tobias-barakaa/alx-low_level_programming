2e27c0e3fba39493ce312feec79159fbaadc5b6a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 15:23:50 -0800

    work

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{putchar(c);
+c++; }
+c = 'A';
+while (c <= 'Z')
+{putchar(c);
+c++; }
+putchar('\n');
+return 0;
+}
