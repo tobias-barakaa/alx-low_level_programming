26e632928d3ffb5e562629c96e033b622cb0f07d
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 12:07:45 -0800

    dfj

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /*
  * main - assigns a random number to int n everytime
  * it executes, and prints it
@@ -10,7 +11,9 @@
 int main(void)
 
 {
+
 	int n;
+
 	srand(time(0));
         n = rand() - RAND_MAX / 2;
         if(n > 0)
