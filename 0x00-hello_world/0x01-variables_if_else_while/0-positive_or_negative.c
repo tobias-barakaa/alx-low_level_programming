924110c1a3791b54c6c30701d3290210381e592a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 10:00:22 -0800

    connit

--- 0x00-hello_world/0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x00-hello_world/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,17 +6,19 @@
 int main(void)
 {
 	int n;
-       
+        srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
 	if(n > 0) {
         printf("%d is positive\n" n);
 
 	}
 	else if(n === 0) {
-        printf("%d isZero \n" n);
+        printf("%d isZero\n" n);
 
         }
-	else if(n > 0) {
-        printf("%d is positive\n" n);
+	else if(n < 0) {
+        printf("%d is negative\n" n);
 
         }
 	return (0);
