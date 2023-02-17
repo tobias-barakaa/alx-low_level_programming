09c461aff8f4227e8e7cef396e475c67ffb40847
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 13:50:04 -0800

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,13 +13,13 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n > 5){
-printf("Last digit of %d is %d and is greater than 5\n", n);
+if((n % n) > 5){
+printf("Last digit of %d is %d and is greater than 5\n", n, n % n);
 }
-else if(n == 0){
-printf("Last digit of %d is %d and is 0\n", n);
+else if((n % n) == 0){
+printf("Last digit of %d is %d and is 0\n", n, n% n);
 }
-else if(n < 6){
+else if((n % n) < 6 && (n % n) !=0 ){
 printf("Last digit of %d is %d less than 6 and not 0\n", n);
 }
 else {
