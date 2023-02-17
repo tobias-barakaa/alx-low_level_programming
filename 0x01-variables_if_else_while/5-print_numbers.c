06465c4e98ef0be0e83ef36c1e2e513b1f0e45fa
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 15:54:40 -0800

    volume

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++) {
+        putchar(i + '0');
+    }
+
+    putchar('\n');
+    return 0;
+}
