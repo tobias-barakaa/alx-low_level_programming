023a462932f4166d3ea237a4e43bfc43dd30e75e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 10:14:56 -0800

    spaces

--- 0x00-hello_world/0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x00-hello_world/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n = rand() - RAND_MAX / 2;
+        int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	if(n > 0) {
         printf("%d is positive\n" n);
 
 	}
-	else if(n === 0) {
+	else if(n == 0) {
         printf("%d isZero\n" n);
 
         }
@@ -21,3 +26,4 @@ int main(void)
         }
 	return (0);
 }
+
