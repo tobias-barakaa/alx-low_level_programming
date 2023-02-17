0a6587b41f1dde99dd2ff2959593e8c7f2b2e634
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 15:00:21 -0800

    cmm

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - assigns a random number to int n everytime
+ * main-assigns a random number to int n everytime
  * it executes, and prints it
  * Return: Always 0 (Success)
  */
