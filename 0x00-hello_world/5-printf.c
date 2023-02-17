98741cb9d9697010a901208294723afe5e53643d
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-16 17:21:41 -0800

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	 printf("with proper grammar, but the outcome is a piece of art,\n");
-	
-	 return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+
+        return (0);
 }
