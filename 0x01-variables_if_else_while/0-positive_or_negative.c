7b3617455bc8d41a63d10065b8c6c321a2d8d79c
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 10:52:18 -0800

    dfj

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,13 +12,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
     if(n > 0) {
-        printf("%d is positive 10\n" n);
+        printf("%d is positive 10\n", n);
     }
     else if(n == 0) {
-        printf("%d is negative\n" n);
+        printf("%d is negative\n", n);
     }
     else if(n < 0) {
-     printf("%d is zero\n" n);
+     printf("%d is zero\n", n);
     }
 	
 	return (0);
