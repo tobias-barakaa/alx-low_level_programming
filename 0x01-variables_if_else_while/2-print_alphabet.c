3f748f6a37f2448ab6d1e0d3a1d30316226901db
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 14:34:01 -0800

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,11 @@
  * Return: Always 0 (Success)
  */
 int main() {
-    char c = 'a';
-    while(c <= 'z') {
-        putchar(c);
-        c++;
-    }
-    putchar('\n');
-    return 0;
+char c = 'a';
+while(c <= 'z') {
+putchar(c);
+c++;
+}
+putchar('\n');
+return 0;
 }
