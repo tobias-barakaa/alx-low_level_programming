b7c52aeebe309e559cdec074e4851a7f13d3b98d
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 14:38:15 -0800

    copy

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,8 @@
  * it executes, and prints it
  * Return: Always 0 (Success)
  */
-int main(void) {
-// This function prints the lowercase alphabet followed by a new line
+int main(void)
+{
 char c = 'a';
 while(c <= 'z') {
 putchar(c);
