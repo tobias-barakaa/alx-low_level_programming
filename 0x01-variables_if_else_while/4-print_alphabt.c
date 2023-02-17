24b69ac2d68ea6bf178f62d803d663fb5e6d5aae
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 15:43:56 -0800

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = "a"
+while(c <= "z")
+{
+if (c !="e" && c !="q") {
+putchar(c)
+}
+c++;
+}
+putchar('\n');
+return 0;
+}	
