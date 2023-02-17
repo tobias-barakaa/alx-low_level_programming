7b4aa202a9c2348beb54dad59e98b95600236420
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 15:25:32 -0800

    print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,11 @@ int main(void)
 {
 char c = 'a';
 while (c <= 'z')
-{putchar(c);
+{ putchar(c);
 c++; }
 c = 'A';
 while (c <= 'Z')
-{putchar(c);
+{ putchar(c);
 c++; }
 putchar('\n');
 return 0;
