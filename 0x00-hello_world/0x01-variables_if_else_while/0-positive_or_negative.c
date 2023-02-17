56c962b2c54d26978155617c8f630ef11052faf4
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 09:56:49 -0800

    Printf

---
+++ 0x00-hello_world/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+       
+	if(n > 0) {
+        printf("%d is positive\n" n);
+
+	}
+	else if(n === 0) {
+        printf("%d isZero \n" n);
+
+        }
+	else if(n > 0) {
+        printf("%d is positive\n" n);
+
+        }
+	return (0);
+}
