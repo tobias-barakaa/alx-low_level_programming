bfc3ef37e049c69ea4d5da2f5557c54a91f6bd4c
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 16:14:21 -0800

    git lone

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,17 +8,13 @@
  */
 int main(void)
 {
-    putchar('0');
-    putchar('1');
-    putchar('2');
-    putchar('3');
-    putchar('4');
-    putchar('5');
-    putchar('6');
-    putchar('7');
-    putchar('8');
-    putchar('9');
-    putchar('\n');
+	int n;
 
-    return 0;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
 }
+
