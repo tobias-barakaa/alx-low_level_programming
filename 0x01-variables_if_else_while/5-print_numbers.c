d200a1a51e8a0f7b7e79e84fe0305641c0a6575a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-17 16:00:42 -0800

    commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,9 +10,8 @@ int main(void)
 {
 int i;
 
-for (i = 0; i < 10; i++) {
-putchar(i + '0');
-}
-putchar('\n');
-return 0;
+for (i = 0; i < 10; i++)
+{putchar(i + '0');
+} putchar('\n');
+return (0);
 }
