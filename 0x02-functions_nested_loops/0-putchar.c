0fce2840bf750bb9a6150d6e81bc4efb25a6e2c0
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-21 06:07:18 -0800

    loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,6 +9,15 @@
  */
 int main(void)
 {
-	_putchar("_putchar \n");
-	return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar(' ');
+    _putchar('\n');
+    return (0);
 }
