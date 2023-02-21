b9ea3fbe5d37ff67b3d29e0e82fdbdbff4aae5d2
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-21 07:01:05 -0800

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - assigns a random number to int n everytime
  * it executes, and prints it
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
+
     _putchar('_');
     _putchar('p');
     _putchar('u');
@@ -17,7 +19,6 @@ int main(void)
     _putchar('h');
     _putchar('a');
     _putchar('r');
-    _putchar(' ');
     _putchar('\n');
     return (0);
 }
