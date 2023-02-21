5cab3afcc3dd58a563b655604e7cb806c84b6f8f
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-21 15:27:29 -0800

    betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 -  a function that prints the alphabet in lowercase then ten times
+ * print_alphabet_x10 - a function that prints the alphabet
  *
  * Return: Always 0
  */
