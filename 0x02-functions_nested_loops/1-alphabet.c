8e4f538154fcf720dba6984f218ffb6de4f3cfe7
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-21 09:04:52 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+	for(alphabet = "a"; alphabet =< "z"; alphabet++) {
+		_putchar("%c\n", alphabet);
+	}
+	_putchar("\n");
+	return(0);
+}
