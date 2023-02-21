a0a1ebbc56eab246c5adf5c299ae7f6005efbbdd
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-21 09:07:07 -0800

    alhp

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	char alphabet;
-	for(alphabet = "a"; alphabet =< "z"; alphabet++) {
+	for(alphabet = "a"; alphabet <= "z"; alphabet++) {
 		_putchar("%c\n", alphabet);
 	}
 	_putchar("\n");
