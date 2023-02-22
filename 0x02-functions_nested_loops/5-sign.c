fd03bd66ed54e2c60d8b9f1bd0a462aa35536c0f
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-22 05:31:31 -0800

    add

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,19 +7,19 @@
  */
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                _putchar ('+');
-                return (1);
-        }
-        else if (n == 0)
-        {
-                _putchar ('0');
-        }
-        else if (n < 0)
-        {
-                _putchar('-');
-                return (-1);
-        }
-        return (0);
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
 }
