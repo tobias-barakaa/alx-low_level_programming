69b51ea808d5ece3da0508bec1ebc26680babe61
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-22 00:57:20 -0800

    return

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,12 +8,12 @@
 
 int _isalpha(int c)
 {
-        if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
