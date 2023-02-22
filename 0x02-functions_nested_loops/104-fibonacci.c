ba6cfaacab540eebbc346f32bf448b7b99e9f0aa
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-21 18:57:10 -0800

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,23 +7,44 @@
  */
 int main(void)
 {
-	long int f, s, sum, tot;
+	long int f, s, c, sum, halfaf, halfbf, halfas, halfbs;
+	long int printfhalf, printshalf;
 
 	sum = 0;
 	f = 0;
 	s = 1;
-	tot = 0;
-	while (sum < 4000000)
+	for (c = 0; c < 91; c++)
 	{
 		sum = f + s;
-		if (sum % 2 == 0)
-		{
-			tot += sum;
-		}
+		printf("%ld", sum);
+		if (c != 97)
+			printf(", ");
 		f = s;
 		s = sum;
 	}
-	printf("%ld\n", tot);
+	halfaf = f / 1000000000;
+	halfbf = f % 1000000000;
+	halfas = s / 1000000000;
+	halfbs = s % 1000000000;
+	while (c < 98)
+	{
+		printfhalf = halfaf + halfas;
+		printshalf = halfbf + halfbs;
+		if (printshalf >= 1000000000)
+		{
+			printshalf %= 1000000000;
+			printfhalf++;
+		}
+		printf("%ld%ld", printfhalf, printshalf);
+		if (c == 97)
+			break;
+		printf(", ");
+		halfaf = halfas;
+		halfbf = halfbs;
+		halfas = printfhalf;
+		halfbs = printshalf;
+		c++;
+	}
+	printf("\n");
 	return (0);
 }
-
