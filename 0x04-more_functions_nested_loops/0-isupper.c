1735ac96810b935b2a71701df94c72ebb1e98fac
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 14:01:42 -0800

    proto-type

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - functions to detect uppercase
+ * @c: first argument
+ * Return: always Return 0
+ */
+
+int _isupper(int c)
+{'
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isupper(int c);
+
+
+#endif /* MAIN_H */
