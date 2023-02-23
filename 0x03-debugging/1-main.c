0b1a246a03b6b49a511b84c328ee27ed1663abc0
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 08:26:26 -0800

    block code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,17 +8,17 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
-	
-	/* i = 0;
-	 
-	while (i < 10)
+
+	i = 0;
+
+	/*while (i < 10) 
 	{
-	putchar(i);
-	}
+	      putchar(i);
+	} 
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
