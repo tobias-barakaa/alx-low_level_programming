8a379a6838638391bea1bf9d9c1c5185a987d8cc
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 14:31:32 -0800

    password

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,14 +8,11 @@
  */
 
 int _isupper(int c)
-{'
+{
 
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else 
-	{
-		return (0);
-	}
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
