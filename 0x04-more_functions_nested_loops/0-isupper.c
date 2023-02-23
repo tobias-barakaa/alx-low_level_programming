f887e2644ea8545cf152e1f18d3716cbea8a3387
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 15:14:52 -0800

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - checks if parameter is an uppercase character.
+ * _isupper - detects if c is uppercase
  * @c: input character.
  * Return: 1 if is an uppercase character, 0 in other case.
  */
