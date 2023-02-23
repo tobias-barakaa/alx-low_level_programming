7ba1cad5d7a19db92f2d091fe6b735922cf30a55
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 07:10:50 -0800

    commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - function to run negative or positive
+ *
+ * @int - first argument
+ * Return: Return 0 Always
+ */
+
+void positive_or_negative(int i)
+{
+	if(i < 0) 
+	{
+		printf("%d is negative", i)
+	} else {
+		printf("%d is positive", i)
+	}
+}
