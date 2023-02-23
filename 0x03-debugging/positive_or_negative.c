2c751c5ac51f3b43f8bc8d0e6b0ea1373e4ad148
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 07:19:21 -0800

    positive or negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,16 +3,18 @@
 /**
  * positive_or_negative - function to run negative or positive
  *
- * @int - first argument
+ * @i: first argument
  * Return: Return 0 Always
  */
 
 void positive_or_negative(int i)
 {
-	if(i < 0) 
+	if (i < 0)
 	{
-		printf("%d is negative", i)
-	} else {
-		printf("%d is positive", i)
+	printf("%d is negative", i);
+	}
+	else
+	{
+	printf("%d is positive", i);
 	}
 }
