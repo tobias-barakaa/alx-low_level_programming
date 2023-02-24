b1228f97156cd96e82bf32a238d0a48a3501d307
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 18:18:15 -0800

    interger

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_number - prints interger
+* @n: param
+*/
+
+void print_number(int n)
+{
+unsigned int i, j, count;
+if (n < 0)
+{
+_putchar(45);
+i = n * -1;
+}
+else
+{
+i = n;
+}
+j = i;
+count = 1;
+while (j > 9)
+{
+j /= 10;
+count *= 10;
+}
+for (; count >= 1; count /= 10)
+{
+_putchar(((i / count) % 10) + 48);
+}
+}
