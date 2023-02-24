0402beae5b72e23b52cdc1b45bfb17ef658a4e95
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 17:02:33 -0800

    producer

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - function prints from 0 to 9
+ *
+ * Return: Return alwasy 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	_putchar(i + '0')
+	}
+	_putchar("\n")
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,5 +6,6 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif /* MAIN_H */
