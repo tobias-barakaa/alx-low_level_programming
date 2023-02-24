4f2619154503b6a7b4475d7085e58dc74fbe6c67
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 17:12:49 -0800

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - detect number, from 0 to 9,
+ * print_numbers - detect the numbers, from 0 to 9,
  * followed by a new line
  */
 void print_numbers(void)
