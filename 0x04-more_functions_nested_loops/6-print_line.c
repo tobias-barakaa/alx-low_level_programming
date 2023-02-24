db02d7c32b17fc4b06c77f5a995ddff94a18b66c
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 17:40:23 -0800

    scar

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - terminal print straight lines
+ * @n: number of times
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
