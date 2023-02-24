571eab33179d2703ca2aaca5305c59cf4c62986e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 16:13:20 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiple of intergers
+ *
+ * @a: first argument
+ * @b: second argument
+ */
+
+int mul(int a, int b)
+{
+	return a * b
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,5 +5,6 @@
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif /* MAIN_H */
