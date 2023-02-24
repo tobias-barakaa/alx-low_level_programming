f63b41efafbe0d7da53fc86ddbc6796140fed5d9
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 17:09:24 -0800

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_numbers - function prints from 0 to 9
- *
- * Return: Return alwasy 0
+ * print_numbers - detect number, from 0 to 9,
+ * followed by a new line
  */
-
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-	_putchar(i + '0')
+		_putchar(i + '0');
 	}
-	_putchar("\n")
+
+	_putchar('\n');
 }
