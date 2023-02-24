79dcf1fd09a84169c75c8cd8adf8f8867c7ec317
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 18:11:48 -0800

    push

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,34 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - prints a hunderd fizz characters in multiples
- * Return: Always 0 
+ * main - prints number of 3 and 5.
+ * Return: Always 0.
  */
+
 int main(void)
 {
-	int i;
-
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf(" FizzBuzz");
-		} else if (i == 1)
-		{
-			printf("%d", i);
-		} else
-		{
-			printf(" %d", i);
-		}
-	}
-	printf("\n");
-
-	return (0);
+int n;
+n = 1;
+printf("%d", n);
+for (n = 2; n <= 100; n++)
+{
+if ((n % 3 == 0) && (n % 5 == 0))
+{
+printf(" FizzBuzz");
+}
+else if (n % 3 == 0)
+{
+printf(" Fizz");
+}
+else if (n % 5 == 0)
+{
+printf(" Buzz");
+}
+else
+{
+printf(" %d", n);
+}
+}
+printf("\n");
+return (0);
 }
