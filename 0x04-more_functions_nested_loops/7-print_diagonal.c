b0fd013e595690abf71649db0b5538becfe38a88
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 17:47:43 -0800

    kum-baya

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line 
+ *  * @n: number of times 
+ * Return: Return 0 always
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
