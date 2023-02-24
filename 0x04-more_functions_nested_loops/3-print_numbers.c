56951f026cf35beea23289aa4b0089ba90332b17
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-23 17:21:44 -0800

    monk

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - detect the numbers, from 0 to 9,
+ * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
  */
 void print_numbers(void)
