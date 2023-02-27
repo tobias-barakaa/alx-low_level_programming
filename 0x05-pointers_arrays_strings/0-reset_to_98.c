2f6aa3e32a4d2adee87f6d1405198f3eea39b5dc
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-27 10:41:32 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function to update int to 98
+ * @*n: asterik argument 
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
