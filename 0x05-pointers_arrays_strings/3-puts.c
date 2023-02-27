7e5e8cc9cd68297220aa726b093c368c1134bada
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-27 14:25:28 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints string followed new line
+ * @str: first argument
+ * Return: Always return 0
+ */
+
+void _puts(char *str)
+{
+	char j[] = "string";
+	puts("%s\n" str[j]);
+}
