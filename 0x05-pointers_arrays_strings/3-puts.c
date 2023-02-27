b2b0092bd4e3977e24509519c665c8f0e62a9481
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-27 14:48:29 -0800

    tabs

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - function that prits
- * @str: first argument
- * Return: Always return 0
+ * _puts - prints stdt
+ * @str: arguments
  */
-
 void _puts(char *str)
 {
-	int z;
+        int z;
 
-	for (z = 0; str[z] != '\0'; z++)
-	 {
-		_putchar(str[z]);
-	}
-	 _putchar('\n');
+        for (z = 0; str[z] != '\0'; z++)
+        {
+                _putchar(str[z]);
+        }
+        _putchar('\n');
 }
