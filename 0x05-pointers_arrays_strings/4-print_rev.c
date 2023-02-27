aeee095fb8dadd54ca16673a32d3409bcb8083ba
tobias-barakaa <tobiasbarakan@gmail.com>
2023-02-27 15:22:00 -0800

    believe

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - function that check for 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char *str;
+
+        str = "I do not fear";
+        print_rev(str);
+        return (0);
+}
