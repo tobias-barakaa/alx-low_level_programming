9f740e4d5cc6a479366e93b47c9d6117a4d41f1c
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-02 11:22:04 -0800

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,24 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * _strcat - concatenate a string 
+ * @char: first argument
  * Return: Always 0.
  */
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	char *ptr = dest;
+	while (*ptr != "\0")
+	{
+		ptr++;
+	}
+	while (*src != '\0') {
+        *ptr = *src;
+        ptr++;
+        src++;
+    }
+
+	*ptr = '\0';
 	return dest;
 }
