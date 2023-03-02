6be7614c39677524f996f800a68f1bc7b120d418
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-02 15:25:41 -0800

    produce

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,31 @@
 #include "main.h"
-/**
- *_memcpy - a function that copies memory area
- *@dest: memory where is stored
- *@src: memory where is copied
- *@n: number of bytes
- *
- *Return: copied memory with n byted changed
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	int i = 0;
-	int j = 0;
 
-	while (n > 0)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-		n--;
-	}
-	return (dest);
+/**
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+        int i, j;
+
+        i = 0;
+        j = 0;
+
+        while (dest[i] != '\0')
+                i++;
+
+        while (src[j] != '\0' && j < n)
+        {
+                dest[i] = src[j];
+                i++;
+                j++;
+        }
+
+        dest[i] = '\0';
+
+        return (dest);
 }
