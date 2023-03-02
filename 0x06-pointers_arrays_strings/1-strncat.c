03c334ab6630effa9896e364b39c927f5147d906
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-02 15:29:25 -0800

    add

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,22 +10,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-        int i, j;
-
-        i = 0;
-        j = 0;
-
-        while (dest[i] != '\0')
-                i++;
-
-        while (src[j] != '\0' && j < n)
-        {
-                dest[i] = src[j];
-                i++;
-                j++;
-        }
-
-        dest[i] = '\0';
-
-        return (dest);
+	int i, j;
+	
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
