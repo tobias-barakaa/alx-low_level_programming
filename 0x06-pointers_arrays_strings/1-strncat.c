eea533dc65a538d04ff421e664ee53db5f310d9b
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-02 14:57:57 -0800

    sele

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,17 @@
 #include "main.h"
 /**
- *_memcpy - a function that copies
- *@dest: argument that stores
- *@src: argument second
- *@n: number of bytes
- *
- *Return: Always return 0
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
-	int j = 0;
+	unsigned int i;
 
-	while (n > 0)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-		n--;
-	}
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+
 	return (dest);
 }
