c07dd5ccd8f6b8022fe15ab1b711abdbbff58c62
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-02 10:49:19 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return dest;
+}
