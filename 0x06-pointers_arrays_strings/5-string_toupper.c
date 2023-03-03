2d72f420eb402cbd59225b9c8c1de1710fbf10f5
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-02 16:15:35 -0800

    five

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - change string to uppercase.
+ * @str: The string to be changed.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
+}
