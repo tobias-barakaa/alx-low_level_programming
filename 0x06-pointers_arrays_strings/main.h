27f7ce4eacb96d200ca167c06a77bdfb3fc5a979
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-02 16:47:02 -0800

    favor

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,3 +13,6 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif
