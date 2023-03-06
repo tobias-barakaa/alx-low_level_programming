9be39923d1ffb69541a2633d9703f6180d742a8a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-06 15:06:56 -0800

    commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memset - prints buffer in hexa
  * @s: the address of memory to print
@@ -7,14 +6,12 @@
  * @b: the data type to print
  * Return: Nothing.
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
- unsigned char* p = s;
-    unsigned char v = c;
-     for (int i = 0; i < n; i++)
-     buffer[i] = " ";
- 
-      for (int i = 0; i < n; i++)
-     _putchar("%c", buffer[i]);
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
 }
