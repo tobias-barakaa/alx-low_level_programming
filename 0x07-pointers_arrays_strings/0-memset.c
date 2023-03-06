828a18446dadab049c8bd18dd2f077cc953c8dbc
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-06 14:05:54 -0800

    pressure

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memset - prints buffer in hexa
  * @s: the address of memory to print
@@ -7,10 +6,9 @@
  * @b: the data type to print
  * Return: Nothing.
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-       while (n > 0)
+	while (n > 0)
 	{
 	 s[n - 1] = b;
 	n--;
