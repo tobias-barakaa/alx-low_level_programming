843f489f97233c67dde4ca56807454b8e60bf39d
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-06 17:39:23 -0800

    commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,18 @@
 #include "main.h"
 
 /**
-* _strspn - gets the length of a prefix substring
-* @s: input string to search for substring
-* @accept: characters tha prefix substring must include
-* Return: length of prrefix substring
+* _strchr - locates a character in a string
+* @s: string input
+* @c: character to find
+* Return: pointer to first occurence of c character
 */
 
-unsigned int _strspn(char *s, char *accept)
+char *_strchr(char *s, char c)
 {
-	unsigned int i, j, a_len = 0, len = 0;
+	unsigned int i;
 
-	while (accept[a_len] != '\0')
-		a_len++;
-	for (i = 0 ; s[i] != '\0'; i++)
-		for (j = 0 ; j < a_len ; j++)
-			if (s[i] == accept[j])
-				len++, j = a_len;
-			else
-				if (j == a_len - 1)
-					goto exit;
-exit: return (len);
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			break;
+	return (s[i] == c ? (s + i) : '\0');
 }
