28afd0c48c80c8952be468b20f7929149876edfa
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-08 14:35:33 -0800

    kfj

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
 
 void _puts_recursion(char *s)
 {
- if (*s == '\0')
- {
-	 putchar('\n');
-	 return;
- }
- putchar(*s);
- _puts_recursion(s ++); 
+	if (*s == '\0')
+	{
+		putchar('\n');
+	       	return;
+	}
+	putchar(*s);
+       	_puts_recursion(s + 1);
 }
