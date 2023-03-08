960f3f56fee3e4c1ac4d2a323fa30f39a70585ea
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-08 15:05:32 -0800

    betty

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - function to print in reverse
+ * @s: last argument
+ * Return: always zero
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_print_rev_recursion(s - 1);
+}
