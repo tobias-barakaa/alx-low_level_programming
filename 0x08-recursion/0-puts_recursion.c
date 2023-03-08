479c22fb8a1ab6626703ae2a1adf29221f909dd5
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-08 14:25:12 -0800

    commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function to print words in new line
+ * @s: first argument
+ * Return: zero always
+ */
+
+void _puts_recursion(char *s)
+{
+ if (*s == '\0') {   
+    putchar('\n');
+    return;
+  }
+  putchar(*s);       
+  _puts_recursion(s + 1); 
+}
