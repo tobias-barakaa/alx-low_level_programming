58d1ab3d63fefde1ef93ca639b2bc0f0575ed38a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-08 14:29:28 -0800

    commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,11 @@
 
 void _puts_recursion(char *s)
 {
- if (*s == '\0') {   
-    putchar('\n');
-    return;
-  }
-  putchar(*s);       
-  _puts_recursion(s + 1); 
+ if (*s == '\0')
+ {
+	 putchar('\n');
+	 return;
+ }
+ putchar(*s);
+ _puts_recursion(s ++); 
 }
