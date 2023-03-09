817ee0376be5b5d242b85aa9b44e6096201594e3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 15:03:42 -0800

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,10 +10,13 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (x != 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
 	{
 		return (-1);
 	}
-	else if (x != 0)
          return (x *( _pow_recursion(y,x - 1)));
 }
