77c8b7b0a43beb009e71e65d3b21a5bc652ff42e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 15:41:38 -0800

    coj

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -19,11 +19,8 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-	else if (x == x)
+	else
 	{
-		return;
-	}
-	else {
-	return x *( _pow_recursion(x,y - 1));
+	 return x *( _pow_recursion(x,y - 1));
 	}
 }
