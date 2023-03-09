33459253e828c06bf87f29a7286349e0be8691af
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 12:29:50 -0800

    commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - find the factorial of a number
+ * @n: number to find factorial
+ * Return: always zero return
+ */
+int fac();
+
+int factorial(int n)
+{
+
+	if (n < 0)
+	{
+	       return (0);
+	}
+	return (n * (factorial(n - 1)));
+}
