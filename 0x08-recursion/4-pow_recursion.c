278138da578547a739a5a2c730e1e2cafbcdde2e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 14:59:49 -0800

    drugs

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,17 +10,10 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (x != 0)
-        {
-                return (1);
-        }
-        else if (x < 0)
-        {
-                return (-1);
-        }
-        else if (y < 0)
-        {
-                return (-1);
-        }
-        return (x *( _pow_recursion(y,x - 1)));
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (x != 0)
+         return (x *( _pow_recursion(y,x - 1)));
 }
