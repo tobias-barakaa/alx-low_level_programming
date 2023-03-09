f035b0fee0683ed438ab8716fa7855bb97f5a0ac
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 12:48:19 -0800

    commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,14 +6,13 @@
  * @n: number to find factorial
  * Return: always zero return
  */
-int fac();
 
 int factorial(int n)
 {
 
-	if (n <= 0)
+	if (n == 0)
 	{
-	       return(-1);
+	       return(1);
 	}
 	return (n * (factorial(n - 1)));
 }
