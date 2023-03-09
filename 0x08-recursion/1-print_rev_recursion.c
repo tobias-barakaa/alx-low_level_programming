8ac6a6a3ac95bcd6c2455c040bf6f37770f85972
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-08 16:11:30 -0800

    commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,11 +9,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if(*s == "\0")
-	{
-		return;
-	}
-	_print_rev_recursion(s + 1);
-	putchar(*s);
-	
+	if (*s == '\0') {
+    return;
+  }
+  _print_rev_recursion(s + 1);
+  putchar(*s);
 }
