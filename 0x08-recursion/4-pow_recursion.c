a7d2c0bd8eaed520071fda7f31ff5b226d720f1c
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 13:16:14 -0800

    function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - function exponential
+ * @x: first argument
+ * @y: second argument
+ * Return: always zero
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int result;
+	if (x != 0)
+	{
+		result += y;
+		       --x;
+	}
+	return result;
+}	
