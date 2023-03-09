46b4d3087a9c3f7c6f5fd94f3834422d0d36a82e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-08 16:18:50 -0800

    whitespace

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,9 +9,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0') {
-    return;
-  }
-  _print_rev_recursion(s + 1);
-  putchar(*s);
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(*s);
 }
