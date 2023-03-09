734674e44599a3d4c74c6076ec859573e92f1987
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 13:00:16 -0800

    buddy

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,15 +9,14 @@
 
 int factorial(int n)
 {
-
 	if (n < 0)
 	{
-	       return (-1);
+		return (-1);
 	}
 	else if (n == 0)
 	{
 		return (1);
 	}
 	else
-	return (n * (factorial(n - 1)));
+		return (n * (factorial(n - 1)));
 }
