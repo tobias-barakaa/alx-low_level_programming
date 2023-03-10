48035546aca555030905f39c45e2673570d73eae
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-10 15:51:17 -0800

    comm

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Convert to absolute value of an integer
+ * @n: the integer to affect
+ *
+ * Return: Return absolute value of integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+}
