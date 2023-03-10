2f33cee0b11b149166257406b7525b5355b1849c
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 18:59:57 -0800

    com

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,42 +1,33 @@
 #include "main.h"
-/**
-*check_prime - get the prime for n
-*@n: num we check is prime or not
-*@i: num we walk through to check the prime
-*Return: 0 if not prime, 1 if prime
-*/
-int check_prime(int n, int i)
-{
-	if (i < n)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-		else
-		{
-			return (check_prime(n, i + 1));
-		}
-	}
-	else
-	{
-		return (1);
-	}
 
-}
+int actual_sqrt_recursion(int n, int i);
+
 /**
-*is_prime_number - fun that we know if num is prime or not
-*@n: num we check
-*Return: 1 if prime, 0 otherwise
-*/
-int is_prime_number(int n)
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root
+ */
+int _sqrt_recursion(int n)
 {
-	if (n <= 0 || n == 1)
-	{
-		return (0);
-	}
-	else
-	{
-		return (check_prime(n, 2));
-	}
+        if (n < 0)
+                return (-1);
+        return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+        if (i * i > n)
+                return (-1);
+        if (i * i == n)
+                return (i);
+        return (actual_sqrt_recursion(n, i + 1));
 }
