8c63ccd6b945e414799692299ea6f24cfdb6a60b
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-10 14:59:48 -0800

    fifty

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * * @str: argument
+ * Return: Return always 0
+ */
+void _puts(char *str)
+{
+	int z;
+
+	for (z = 0; str[z] != '\0'; z++)
+	{
+		_putchar(str[z]);
+	}
+	_putchar('\n');
+}
