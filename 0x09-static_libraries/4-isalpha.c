239967f3b6a22ae3c2c2292e51ae560a973e5e5f
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-10 15:33:04 -0800

    mig

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that returns one if its a letter
+ * @c: a variable
+ * Return: Return one if uppercase else zero
+ */
+
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
