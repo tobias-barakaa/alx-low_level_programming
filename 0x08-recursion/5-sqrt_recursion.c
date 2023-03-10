390651b9408418f5e0921414818fd78a98b55b08
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 17:30:04 -0800

    recurstion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,11 +7,35 @@
  * Return: always zero
  */
 
+int srt_root(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else if (i * i < n)
+	{
+		return (srt_root(n, i + 1));
+	}
+	else
+	{
+		return (i);
+	}
+}
+/**
+*_sqrt_recursion - root of n
+*@n: argument
+*Return: zero always
+*/
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
 		return (-1);
 	}
-	return _sqrt_recursion(n * n);
+	else
+	{
+		return (srt_root(n, 0));
+	}
 }
