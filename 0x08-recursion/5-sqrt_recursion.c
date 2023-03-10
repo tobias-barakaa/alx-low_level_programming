71cd0e7318dac16a1455a1f38c24cc3ee863c597
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 19:05:35 -0800

    add

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,9 +10,9 @@ int actual_sqrt_recursion(int n, int i);
  */
 int _sqrt_recursion(int n)
 {
-        if (n < 0)
-                return (-1);
-        return (actual_sqrt_recursion(n, 0));
+if (n < 0)
+return (-1);
+return (actual_sqrt_recursion(n, 0));
 }
 
 /**
@@ -25,9 +25,9 @@ int _sqrt_recursion(int n)
  */
 int actual_sqrt_recursion(int n, int i)
 {
-        if (i * i > n)
-                return (-1);
-        if (i * i == n)
-                return (i);
-        return (actual_sqrt_recursion(n, i + 1));
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (actual_sqrt_recursion(n, i + 1));
 }
