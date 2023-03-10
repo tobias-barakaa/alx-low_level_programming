9c21af3275a5c0b148f55d621b73e0570be0f313
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 18:34:30 -0800

    dfj

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+ #include "main.h"
+/**
+ * wildcmp - two strings 
+ * @s1: the first argument
+ * @s2: the second argument
+ *
+ * Return: always 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+        if (*s2 == '\0' && *s1 == '\0')
+                return (1);
+
+        if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+                return (0);
+
+        if (*s1 == *s2)
+                return (wildcmp(s1 + 1, s2 + 1));
+
+        if (*s2 == '*')
+                return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
+        return (0);
+}
