f5338635c307ea89d606a5bc6dc22537d54acb5b
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 18:57:36 -0800

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,9 +10,9 @@ int actual_prime(int n, int i);
  */
 int is_prime_number(int n)
 {
-        if (n <= 1)
-                return (0);
-        return (actual_prime(n, n - 1));
+if (n <= 1)
+return (0);
+return (actual_prime(n, n - 1));
 }
 
 /**
@@ -24,9 +24,9 @@ int is_prime_number(int n)
  */
 int actual_prime(int n, int i)
 {
-        if (i == 1)
-                return (1);
-        if (n % i == 0 && i > 0)
-                return (0);
-        return (actual_prime(n, i - 1));
+if (i == 1)
+return (1);
+if (n % i == 0 && i > 0)
+return (0);
+return (actual_prime(n, i - 1));
 }
