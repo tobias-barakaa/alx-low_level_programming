198f5870f336c7eeba617b57f9c4014fb755b877
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-09 18:45:15 -0800

    commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,17 +8,13 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-        if (*s2 == '\0' && *s1 == '\0')
-                return (1);
-
-        if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-                return (0);
-
-        if (*s1 == *s2)
-                return (wildcmp(s1 + 1, s2 + 1));
-
-        if (*s2 == '*')
-                return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-
-        return (0);
+	if (*s2 == '\0' && *s1 == '\0')
+		return (1);
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
 }
