e525cd74e0804ac2ae91663ec8ad288296be2e1a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-10 14:51:31 -0800

    add

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - a function that finds c and prints
+ * @c: first argument
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
