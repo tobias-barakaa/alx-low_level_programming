cc6b288bfbf1364bca0bc771f75c08cfbc517428
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-10 16:20:59 -0800

    commit

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    const char* str = "\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick";
-    for (int i = 0; str[i] != '\0'; i++) {
-        putchar(str[i]);
-    }
-    return 0;
+	const char* str = "\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick";
+	for (int i = 0; str[i] != '\0'; i++) {
+		putchar(str[i]);
+	}
+	return 0;
 }
