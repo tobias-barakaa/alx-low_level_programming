4eb0f7a09b209b7b8e0fbef376bea4156593dc3a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-12 00:46:48 -0800

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,9 @@
  * @arfv: second parameter argv
  * Return: return zero alwasys
  */
-
-int main(int argc , char **argv)
+int main(int argc, char **argv)
 {
 	if (argc > 0)
-	printf("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 	return (0);
 }
