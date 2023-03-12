ece856d437d65ea769f83017a58fb3630bfee66e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-12 03:07:59 -0700

    hater

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print number of prod
+ * @argv: argrument first
+ * @argc: second params
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+		printf("%d\n", argc);
+			return (0);
+}
