13ee6660cac53c2c4b4d1d06d8606913bed63646
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-12 15:38:42 -0700

    future

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - function to multiply
- * @argv: first param
- * @argc: second param
- * Return: Return always zero
+ * main - prints multiples
+ * @argc: first argument
+ * @argv: argument second
+ * Return: zero always
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	if (argc != 3) {
-        printf("Error\n");
-        return 1;
-    }
-
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-
-    int result = num1 * num2;
-    printf("%d\n", result);
-
-    return 0;
+int numq, numb;
+if (argc == 3)
+{
+numq = atoi(argv[1]);
+numb = atoi(argv[2]);
+printf("%d\n", numq *numb);
+return (0);
+}
+printf("Error\n");
+return (1);
 }
