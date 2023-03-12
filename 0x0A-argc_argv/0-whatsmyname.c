7cdd8e65ed23ae0f35e4b63c91ddfdad36700530
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-11 23:23:17 -0800

    commi

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function to print name
+ * @argc: first parameter argc
+ * @arfv: second parameter argv
+ * Return: return zero alwasys
+ */
+
+int main(int argc , char **argv)
+{
+
+	printf("\n Printing the arguments of a program:%d\n", argc);
+	return (0);
+}
