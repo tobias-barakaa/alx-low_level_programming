c2512b132616bc62965d785a24bdabf5dd22a3b3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-12 09:34:45 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,15 +2,16 @@
 #include "main.h"
 
 /**
- * main - print number of prod
+ * main - print number of index
  * @argv: argrument first
  * @argc: second params
+ * Return: always zero
  */
 
 int main(int argc, char **argv)
 {
 	(void)argv;
-	if (argc >= 0)
+	if (argc > 0)
 		printf("%d\n", argc - 1);
 	return (0);
 }
