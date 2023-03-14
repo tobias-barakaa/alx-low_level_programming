aa838a7abeec4613c896a46c4d8860f123926278
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-14 09:11:30 -0700

    coj

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - function that takes arrays
+ * @size: first  argument function
+ * @c: second argument
+ * Return: return NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+        char *arr;
+	unsigned int i;
+	
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
+}
