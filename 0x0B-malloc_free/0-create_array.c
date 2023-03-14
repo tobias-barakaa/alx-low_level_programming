dbe61962b78616f2c7b55e46b490a92ab66bbac6
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-14 08:46:52 -0700

    add

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *arr;
+unsigned int i;
 
     if (size == 0)
         return NULL;
@@ -21,7 +22,7 @@ char *arr;
     if (arr == NULL)
         return NULL;
 
-    for (unsigned int i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
         arr[i] = c;
 
     return arr;
