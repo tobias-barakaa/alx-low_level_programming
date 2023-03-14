fdbc7625b2a55b9647adf002b5614e3af9153a05
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-14 05:43:00 -0700

    create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,16 +11,17 @@
 
 char *create_array(unsigned int size, char c)
 {
-	int *arr;
-	int i;
-	*arr = (char *) malloc(size * sizeof(char));
-	if (size == 0)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-	{
-		arr[i] = c;
-	}
-	return (arr);
+	
+	if (size == 0) {
+        return NULL; 
+   
+       	}
+    
+    char *arr = (char *) malloc(size * sizeof(char)); 
+    if (arr != NULL) {
+        for (unsigned int i = 0; i < size; i++) {
+            arr[i] = c;
+        }
+    }
+    return arr;
 }
