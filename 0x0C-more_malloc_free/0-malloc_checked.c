e1ee991d05f25ebd489473dece2580e70a44e093
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-16 13:37:22 -0700

    add

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,10 +11,9 @@
 
 void *malloc_checked(unsigned int b)
 {
-        int *ptr;
-
-        ptr = malloc(b);
-        if (ptr == NULL)
-                exit(98);
-        return (ptr);
+	void *ptr = malloc(b);
+	if (ptr == NULL) {
+		exit(98);
+	}
+	return ptr;
 }
