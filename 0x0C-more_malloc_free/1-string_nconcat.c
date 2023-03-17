9d716e1f0792a8ce56e6a98a198586c30c615781
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-17 01:30:00 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,38 +11,42 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *mall;
-	unsigned int ls1, ls2, lmall, i;
+
+	unsigned int i, len1, len2, length, j;
+	char *p;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
-
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
-
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
-
-	if (n > ls2)
-		n = ls2;
-
-	lmall = ls1 + n;
-
-	mall = malloc(lsout + 1);
-
-	if (mall == NULL)
+	}
+	len1 = 0;
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		len1++;
+	len2 = 0;
+	for (i = 0; *(s2 + i) != '\0'; i++)
+		len2++;
+	if (n >= len2)
+	{
+		n = len2;
+	}
+	length = len1 + n;
+	p = malloc((sizeof(char) * length) + 1);
+	if (p == NULL)
+	{
 		return (NULL);
-
-	for (i = 0; i < lmall; i++)
-		if (i < ls1)
-			mall[i] = s1[i];
-		else
-			mall[i] = s2[i - ls1];
-
-	mall[i] = '\0';
-
-	return (sout);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (j = 0; i < length; j++, i++)
+	{
+		p[i] = s2[j];
+	}
+	p[i] = '\0';
+	return (p);
 }
