03aebe245dd0611f56810eba2eb3f03c60cc1263
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-20 16:01:35 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - main function
- * Return: 0
+ * main - print file name
+ * Return:0
  */
-int main()
+
+int main(void)
 {
-printf("%s\n",__FILE__);
+printf("%s\n", __FILE__);
 return (0);
 }
