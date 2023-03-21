f399e5608ebbf8b04ca445619b0e80b34eb7b7a0
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-21 15:11:08 -0700

    add

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,10 +13,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-        if (d != NULL)
-        {
         d->name = name;
         d->owner = owner;
         d->age = age;
-        }
 }
