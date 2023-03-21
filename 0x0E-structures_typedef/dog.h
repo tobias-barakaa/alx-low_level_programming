81ea6c215ea759fa750181ea693359f416e1a627
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-21 11:26:04 -0700

    fj

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,7 @@
 #include <stdio.h>
 
-
-/**
- * main - main function
- * Return: zero always
- */
-
-
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-}
-
-int main(void)
-{
-	struct dog dog;
-	return (0);
-}
+struct dog {
+  char *name;
+  float age;
+  char *owner;
+};
