971cffe7307489e5179d7389b721889d18e15123
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-21 16:56:15 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,23 +2,25 @@
 #define DOG_H
 
 /**
- * struct dog - function info
- * @name: argument
- * @age: param
- * @owner: param
- *
- * Description: Longer description
- */
+* struct dog - function info
+* @name: argument
+* @age: param
+* @owner: param
+*
+* Description: Longer description
+*/
+
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 /**
  * dog_t - typedef for struct dog
  */
+
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
