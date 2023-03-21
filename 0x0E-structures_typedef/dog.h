2a1177d59a6b1aeaccc65520bd95bc64f7b30602
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-21 13:49:08 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,16 +2,18 @@
 #define _HEADER_
 
 /**
- * struct dog - main function
- * @name: name of fun
- * @age: age of fun
- * @owner: owner
- */
+* struct dog - main function
+* @name: name of fun
+* @age: age of fun
+* @owner: owner
+*/
 
-struct dog {
-  char *name;
-  float age;
-  char *owner;
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
 
 #endif
