76154a18c3535e53545e6ff45a1b812c25dd0247
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-21 16:07:40 -0700

    checker

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,18 @@
-#include "dog.h" #include <stdlib.h> /** * init_dog - initialize a variable of type `struct dog` * @d: dog object * @name: string for name * @age: integer for age * @owner: string for owners name */ void init_dog(struct dog *d, char *name, float age, char *owner) { if (d) { d->name = name; d->age = age; d->owner = owner; } }
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+        if (d == NULL)
+                d = malloc(sizeof(struct dog));
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+}
