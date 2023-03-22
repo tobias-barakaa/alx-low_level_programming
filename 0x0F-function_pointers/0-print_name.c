66e7a4d1cfcccaf509c82dc3451086934e4760f3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-22 14:50:56 -0700

    name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
-* print_name - functions name
-* @name: first arg
-* @f: scond arg
-* Return: nothing
-*/
-
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
