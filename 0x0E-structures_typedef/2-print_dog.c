448a492919dfae33c28a6dcc5fd122c102274b67
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-21 18:06:15 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,16 +6,14 @@
  * print_dog - function to print struct dog
  * @d: pointer
  */
+
 void print_dog(struct dog *d)
 {
-        if (d == NULL)
-                return;
-
-        if (d->name == NULL)
-                d->name = "(nil)";
-        if (d->owner == NULL)
-                d->owner = "(nil)";
-
-        printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+if (d == NULL)
+return;
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
-
