9d3dc2d9f79086833f9c1b6d524faddee05e45c2
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-21 17:17:08 -0700

    add

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,12 +15,11 @@ struct dog
 char *name;
 float age;
 char *owner;
-};
+}
 
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
