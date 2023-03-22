cb5931df5e7fd714a262d556e5416c6520fef4c7
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-21 18:15:15 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function to free dogs
+ *
+ * @d: pointer 
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
