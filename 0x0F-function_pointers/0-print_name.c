cc2c3b675af75e64f03760865cd60f1baf190bc5
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-22 13:57:43 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
+#include "function_pointers.h"
 
-#define print_name(x) (sizeof (x), printf("%s\n", #x))
+/**
+ * print_name - functions name
+ * @name: first arg
+ * @f: scond arg
+ * Return: zero always
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
-}
-	void print_name_standard(char *name) {
-    printf("%s\n", name);
-}
-
-int main() {
-    char *name = &name;
-    print_name(name, print_name_standard);
-    return 0;
+if (name && f)
+		f(name);
 }
