77b6f15c6ab6490f351d0563529d354dbe72e658
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-22 14:48:56 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - functions name
- * @name: first arg
- * @f: scond arg
- * Return: zero always
- */
+* print_name - functions name
+* @name: first arg
+* @f: scond arg
+* Return: nothing
+*/
 
 void print_name(char *name, void (*f)(char *))
 {
-
 	if (name && f)
 		f(name);
 }
