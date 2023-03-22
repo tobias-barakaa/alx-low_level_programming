989db219ce14bc13e361c60fdabf5dc4866f3e27
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-22 15:06:05 -0700

    add

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+/* defines size_t */
+
+/**
+ *array_iterator - execute function on an array via a function ptr
+ *@array: array
+ *@size: size of array
+ *@action: pointer to function that we need to execute
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+
+if (array != NULL && action != NULL)
+for (i = 0; i < size; i++)
+action(*(array + i));
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,8 @@
 #define FUNCTION_POINTERS_H
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 #include <stdio.h>
 
 #endif
