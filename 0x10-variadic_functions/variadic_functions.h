41cc6f3aea9e3170b6bf073322e2289cc2099010
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-23 15:18:16 -0700

    sijui

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,27 @@
 #ifndef VARIADIC_FUNCTION.H
 #define VARIADIC_FUNCTION.H
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-#include <stdio.h>
+/**
+ * struct p - checker struct
+ * @p: possibility
+ * @f: Function to be used
+ */
+typedef struct p
+{
+  char *p;
+  void (*f)(va_list);
+} checker;
+
+void print_c(va_list);
+void print_i(va_list);
+void print_f(va_list);
+void print_s(va_list);
+
 
 #endif
