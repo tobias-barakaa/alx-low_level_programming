931d897517fe673877e406a27e66fc2d1336b12b
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-23 14:50:03 -0700

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,16 +15,22 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	 int sum = 0;
-	va_list args;
-		
-		unsigned int i;
-		if (n == 0)
-			return (0);
-		va_start(args, n);
-		for (i = 0; i < n; i++) {
-			sum += va_arg(args, int);
-		}
-		va_end(args);
-		return (sum);
+	 va_list args;
+    int sum = 0;
+    unsigned int i;
+
+    if (n == 0) {
+        return 0;
+    }
+
+    va_start(args, n);
+
+    for (i = 0; i < n; i++) {
+        sum += va_arg(args, int);
+    }
+
+    va_end(args);
+
+    return sum;
+
 }
