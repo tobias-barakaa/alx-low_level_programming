16361eefa4038fa369e7a74f172a4b1e33e92ef3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-30 16:24:12 -0700

    starts

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,33 +9,26 @@
  * @len: length of the string
  * @h: pointer
  * @next: points to the next node
- *
+ * @num: number of return
  * Description: singly linked list node structure
  */
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
 
 size_t print_list(const list_t *h)
 {
-    struct list_s *next;
-    while (h != NULL)
+	size_t num = 0;
+    while(h != NULL)
     {
-        if (h->next != h->str)
-        {
-            printf("[0] (nil)")
-        }
-        else
-        {
-            h->next = h->str;
-            h->str = h->next;
-            h->next = h->len;
-            h->len = h->next;
-            printf("%c, %d", h->str, h->len);
-        }
+	    if(h->str == NULL)
+	    {
+		    printf("[0] (nil)\n");
+	    }
+	    else 
+	    printf("[%d] %s\n", h->len, h->str);
+	    
+	    num++;
+	    h = h -> next;
+
     }
+    return num;
 }
 
