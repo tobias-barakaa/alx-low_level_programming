e8df7b3c816b6da07bab96e05bc4165649aba6dd
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-30 16:46:17 -0700

    print_List

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,30 +5,27 @@
 
 /**
  * print_list - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @h: pointer
- * @next: points to the next node
- * @num: number of return
- * Description: singly linked list node structure
+ * @h: pointer to the next
+ * @next:
+ * @num:
+ *Return:num
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t num = 0;
-    while(h != NULL)
-    {
-	    if(h->str == NULL)
-	    {
-		    printf("[0] (nil)\n");
-	    }
-	    else 
-	    printf("[%d] %s\n", h->len, h->str);
-	    
-	    num++;
-	    h = h -> next;
+size_t num = 0;
 
-    }
-    return num;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+printf("[%d] %s\n", h->len, h->str);
+num++;
+h = h->next;
+}
+return (num);
 }
 
