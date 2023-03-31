95cbc64d5af23215b5d06e0b386b2f39592baaa1
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-30 18:35:57 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
 
 void __attribute__ ((constructor)) before_main()
 {
-        printf("You're beat! and yet, you must allow,\n
-                        I bore my house upon my back!\n");
+        printf("You're beat! and yet, you must allow,\n");
+        printf("I bore my house upon my back!\n");
 }
