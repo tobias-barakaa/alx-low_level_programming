f750ebceaaaad3cf836f301298f3b5170a5b95d4
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-30 18:34:40 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * before_main - before main
+ */
+
+void __attribute__ ((constructor)) before_main()
+{
+        printf("You're beat! and yet, you must allow,\n
+                        I bore my house upon my back!\n");
+}
