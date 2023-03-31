1b82195a98eb38ed1e93e2b4df905616db7d4d1f
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-30 18:19:52 -0700

    parenthesei

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+/**
+ * before_main - before main
+ */
+
 void __attribute__ ((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
