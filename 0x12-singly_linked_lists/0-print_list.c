b32e248e992b5cb1e4281801b23097097cd784f2
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-30 18:44:12 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,21 +7,25 @@
  * print_list - singly linked list
  * @h: pointer to the next
  * @next:
- * @num:
+ * @counter:
  *Return:num
  */
 size_t print_list(const list_t *h)
 {
-size_t num;
-num = 0;
-while (h != NULL)
-{
-if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)");
-else
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-num++;
-}
-return (num);
+
+	const list_t *temp = h;
+	int counter = 0;
+
+	while (temp != NULL)
+	{
+		if (temp->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+			printf("[%d] %s\n", temp->len, temp->str);
+		counter++;
+		temp = temp->next;
+	}
+	return (counter);
 }
