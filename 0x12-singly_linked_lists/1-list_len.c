51a01de697bcb8824b7e8bac92eecf3fb0eb0a03
tobias-barakaa <tobiasbarakan@gmail.com>
2023-03-30 17:29:11 -0700

    list_len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,20 +3,21 @@
 #include <stdio.h>
 #include "lists.h"
 
-
 /**
- * size_t list_len - function list
- * @h: header
- * Return: nodes
+ * list_len - function list
+ * @h: header pointer
+ * @elements:
+ * Return: elem
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
-	while (h != NULL)
-	{
-		++elements;
-		h = h-> next;
-	}
-	return elements;
+size_t elements = 0;
+
+while (h != NULL)
+{
+++elements;
+h = h->next;
+}
+return (elements);
 }
