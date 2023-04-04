b365a5404b57d4fd0d77a0649755cfe84cb95b52
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-04 16:14:18 -0700

    nodejs

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - function to add
+ * @head: pointer to pointer
+ * @n: data to insert
+ * Return: return add
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *head
+}
