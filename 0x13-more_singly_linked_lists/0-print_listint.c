e680dc99a230e6422252823f7401ec93482842ac
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-04 23:25:33 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,20 @@
 #include "lists.h"
 
-
 /**
- * print_listint - function to prints all the
- * elements of a listint_t list
- * @h: header
- * Return: return the elements
- */
+* print_listint - function to prints all the
+* elements of a listint_t list
+* @h: header
+* Return: return the elements
+*/
+
 size_t print_listint(const listint_t *h)
 {
+	size_t = 0;
 	while(h != NULL)
 	{
+		++nod;
 		printf("%d/n", h->n);
 		h = h->next;
 	}
-	return(h);
+	return(nod);
 }
