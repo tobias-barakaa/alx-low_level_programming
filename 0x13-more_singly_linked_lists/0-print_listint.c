3c1d761196e241074f8c4b125d880f8d6cf1ef44
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-04 13:50:32 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int nod = 0;
-	
-	while (h != NULL)
-	{
-		++nod;
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-	return (nod);
+int nod = 0;
+
+while (h != NULL)
+{
+++nod;
+printf("%d\n", h->n);
+h = h->next;
+}
+return (nod);
 }
