59d5f8b38c8f965478092896bcf8e8bbac24046e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-04 12:51:14 -0700

    start

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+
+/**
+ * print_listint - function to prints all the
+ * elements of a listint_t list
+ * @h: header
+ * Return: return the elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int nod = 0;
+	
+	while(h != NULL)
+	{
+		++nod;
+		h = h->next;
+	}
+	printf("%u", nod);
+}
