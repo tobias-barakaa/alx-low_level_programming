24ce19b94350fc10d80ce37e0b836868bac4d536
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-04 23:35:38 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,10 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nod = 0;
 	while(h != NULL)
 	{
-		++nod;
-		printf("%d/n", h->next->n);
+		printf("%d/n", h->n);
 		h = h->next;
 	}
-	return(nod);
+	return(h);
 }
