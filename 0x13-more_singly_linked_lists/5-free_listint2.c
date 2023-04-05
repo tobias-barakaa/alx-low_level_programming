8a3f9d4babf7f6057e0177fbe9f525e23ca8bd9f
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-05 05:27:47 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,20 +7,18 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
-	listint_t *next;
-
-	if (head == NULL)
-		return;
-	if (*head == NULL)
-		return;
-
-	current = *head;
-	while (current != NULL)
-	{
-		next = current->next;
-		free(current);
-		current = next;
-	}
-	*head = NULL;
+listint_t *current;
+listint_t *next;
+if (head == NULL)
+return;
+if (*head == NULL)
+return;
+current = *head;
+while (current != NULL)
+{
+next = current->next;
+free(current);
+current = next;
+}
+*head = NULL;
 }
