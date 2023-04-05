a173a9343b7489e32f6b822f8aa7214beca27747
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-05 12:10:48 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,10 +14,11 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+char *str;
+unsigned int len;
+struct list_s *next;
+}
+list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
