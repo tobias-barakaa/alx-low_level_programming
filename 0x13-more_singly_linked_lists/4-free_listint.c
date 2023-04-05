1b0a323a6453d191b86803ee9c15ba597c20c265
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-05 04:32:37 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,20 +8,11 @@
 
 void free_listint(listint_t *head)
 {
-	if (head->next == NULL){
-		free(head);
-		head = NULL;
-	}
-	else {
-		listint_t *new = head;
-		listint_t *old = head;
-		while (new->next != NULL) {
-			old = new;
-			new = new->next;
-		}
-		old->next = NULL;
-		free(new);
-		new = NULL;
-	}
-	return (0);
+listint_t *node;
+
+while (head != NULL)
+{
+node = head;
+head = head->next;
+free(head);
 }
