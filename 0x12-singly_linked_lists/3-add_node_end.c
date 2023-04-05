ccd7ddc0700695dda2a508d8cba4bc209ad8ce9e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-05 14:57:50 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,32 +4,32 @@
  * add_node_end - adds at the end
  * @head: pointer
  * @str: string
- *
  * Return: address
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newElem;
-	list_t *ptrMove = *head;
+list_t *ptrMove = *head;
 
-	newElem = malloc(sizeof(list_t));
-	if (newElem != NULL)
-	{
-		newElem->str = strdup(str);
-		newElem->len = _strlen(str);
-		newElem->next = NULL;
-	}
-	else
-		return (NULL);
-	if (ptrMove != NULL)
-	{
-		while (ptrMove->next != NULL)
-			ptrMove = ptrMove->next;
-		ptrMove->next = newElem;
-	}
-	else
-		*head = newElem;
-	return (newElem);
+newElem = malloc(sizeof(list_t));
+if (newElem != NULL)
+{
+	newElem->str = strdup(str);
+newElem->len = _strlen(str);
+newElem->next = NULL;
+}
+else
+return (NULL);
+if (ptrMove != NULL)
+{
+	while (ptrMove->next != NULL)
+	ptrMove = ptrMove->next;
+ptrMove->next = newElem;
+}
+else
+*head = newElem;
+return (newElem);
 }
 
 /**
@@ -42,7 +42,7 @@ int _strlen(const char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
-		i++;
-	return (i);
+while (str[i] != '\0')
+	i++;
+return (i);
 }
