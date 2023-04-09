5c20d0e3ead44cb32f6d9481872ee4449c1ba6b9
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-09 15:58:38 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,12 +12,13 @@ unsigned int to_bi;
 
 for (to_bi = 0; *b; b++)
 {
+	if (*b == NULL)
+		return (0);
 	if (*b == '1')
 	to_bi = (to_bi << 1) | 1;
 else if (*b == '0')
 	to_bi <<= 1;
 	else
-		return (0);		
+		return (to_bi);		
 }
-	return (to_bi);
 }
