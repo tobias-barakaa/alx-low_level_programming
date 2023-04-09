1a6af6690f48accb19c9b6b24859afbf28e1ddcb
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-09 05:20:05 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,16 +8,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int to_bit;
-	for (to_bit = 0; *b; b++)
-	{
-		if (*b == '1')
-            to_bit = (to_bit << 1) | 1;
-        else if (*b == '0' )
-            to_bit <<= 1;
-        else
-            return (0);
-    }
+unsigned int to_bit;
 
-    return (to_bit);
+for (to_bit = 0; *b; b++)
+{
+	if (*b == '1')
+	to_bit = (to_bit << 1) | 1;
+else if (*b == '0')
+	to_bit <<= 1;
+else
+	return (0);
+}
+return (to_bit);
 }
