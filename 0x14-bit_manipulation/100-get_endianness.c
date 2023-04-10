98562a136578a630839fbced1741d8a8b8ceddd3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-10 14:40:37 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
-/*
-*get_endianness - checks the end of the code
-*data: data argument first
-*ptr: point to char
-*char: pointer
-*Return: function for endiassness
+/**
+ * get_endianness - checks the end of the code
+ * @data:
+ * @kimba:
+ * Return: function for endiassness
 */
 
 int get_endianness(void)
 {
 unsigned int data = 1;
-  
-  char *kimba = (char *)&data;
-  
-  if (*kimba)
-  {
-		return (1);
-  }
-  return (0);
+
+char *kimba = (char *)&data;
+
+if (*kimba)
+{
+return (1);
+}
+return (0);
 }
