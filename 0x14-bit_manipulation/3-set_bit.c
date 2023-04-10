ef8f31f0da5ddc576fb51b36a42780840620299d
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-10 19:45:05 +0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,1 +1,17 @@
-return one
+#include "main.h"
+
+/**
+*set_bit -sets the value of a bit to 1 at a given index
+*@n: first argument
+*@index: second arg
+*Return: return 1 if success
+*/
+
+
+int set_bit(unsigned long int *n, unsigned int index) {
+    int value_elem_data = (index >= sizeof(unsigned long int) * 8);
+    if (value_elem_data)
+    return (-1);
+    *n |= (1 << index);
+    return (1);
+}
