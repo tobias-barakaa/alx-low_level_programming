aaf46fa00b92b9de1c051dfb2c661365d7fcb976
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-10 18:27:38 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,29 +8,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-int bit;
-unsigned int bitss;
+int bits, i;
 
-}
-
-
-
-
-#include "main.h"
-
-/**
- * get_bit - value of bit
- * @n: number
- * @index: index
- * Return: return val
- */
-int get_bit(unsigned long int n, unsigned int index)
-{
-int bit;
-unsigned int bitss;
-bitss = (sizeof(unsigned long int) * 8);
-if (index > bitss)
-return (-1);
-bit = ((n >> index) & 1);
-return (bit);
+  while(i=(sizeof(unsigned long int) * 8)
+        if (index > i)
+        return (-1);
+        bits = ((n >> index) & 1);
+        return (bits);
 }
