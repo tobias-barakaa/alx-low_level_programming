1101ac73a0b182016b9304d9e126a06969369c75
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-10 09:01:57 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,12 +8,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-int val;
-  unsigned int size_of_elem_val;
-
-  size_of_elem_val = (sizeof(unsigned long int) * 8);
-        if (index >= size_of_elem_val)
-        return (-1);
-        val = ((n >> index) & 1);
-        return (val);
+	int val;
+	unsigned int size_of_elem_val;
+	
+	size_of_elem_val = (sizeof(unsigned long int) * 8);
+	if (index >= size_of_elem_val)
+		return (-1);
+	val = ((n >> index) & 1);
+	return (val);
 }
