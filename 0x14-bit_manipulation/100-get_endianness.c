606a7a388317e01c05e3adccd4f6fd311a6e5e2f
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-10 23:37:42 +0300

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,15 @@
-jfkjdfj
+#include "main.h"
+
+/**
+*get_endianness - checks the end of the code
+*data: data argument first
+*ptr: point to char
+*char: pointer
+*Return: function for endiassness
+
+int get_endianness(void)
+{
+int data = 1;
+char *ptr = (char *) &num;
+return (*ptr == 1);
+}
