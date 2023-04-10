c2ed3acea5715ab6b0a86d17079f3bf09f9f7cb2
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-10 13:22:46 -0700

    bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,1 +1,21 @@
-fkjdfj
+#include "main.h"
+
+/**
+*flip_bits - func to flipp numbers
+*@n: first arg
+*@m: second args
+*Return: return flip bits rtn
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int number_long_integ = n ^ m;
+unsigned int number_elements_sum_digits_data = 0;
+    
+while ((number_long_integ) != 0) 
+{
+number_elements_sum_digits_data += number_long_integ & 1;
+number_long_integ >>= 1;
+}
+return (number_elements_sum_digits_data);
+}
