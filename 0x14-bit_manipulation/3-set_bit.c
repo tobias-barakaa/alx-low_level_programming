3289820e5a3034935abb3e1ff2e6314faa8c5c6e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-10 10:08:40 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int value_elem_data = (index >= sizeof(unsigned long int) * 8);
-	
-	if (value_elem_data)
-		return (-1);
-	*n |= (1 << index);
-	return (1);
+int value_elem_data = (index >= sizeof(unsigned long int) * 8);
+if (value_elem_data)
+return (-1);
+*n |= (1 << index);
+return (1);
 }
