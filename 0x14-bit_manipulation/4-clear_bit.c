7cbce956c7c085cbee5f5f78ed3c9cd93578d739
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-10 10:51:23 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-*clear_bit - func dat sets the value of 
-*a bit to 0 at a given index
+*clear_bit - function dat sets the value of a bit to 0 at a given index
 *@n: first argument
 *@index: index arg
+*Return: -1 if not legit else 1
 */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-  int value_elem_index = (index >= sizeof(unsigned long int) * 8);
-  
-  if(value_elem_index)
-  return (-1);
-  *n &= ~(1 << index);
-  return (1);
+int value_elem_index = (index >= sizeof(unsigned long int) * 8);
+
+if (value_elem_index)
+return (-1);
+*n &= ~(1 << index);
+return (1);
 }
