56d508ede23799513e12318a41dbce61a656ce7a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-10 19:11:07 -0700

    print

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,20 +10,20 @@
 void print_binary(unsigned long int n)
 {
 int new_elem_data;
+
 static int len;
 
 if (n == 0 && len > 0)
- {
+{
 return;
- }
+}
 else if (n == 0)
 {
 _putchar('0');
 return;
 }
-
 new_elem_data = (n & 1);
 len++;
 print_binary(n >>= 1);
 _putchar('0' + new_elem_data);
-}  
+}
