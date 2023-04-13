e6e59ef7e8c844f7ff8768d2e2ef6d4a9650b4af
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-04-14 01:06:53 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,4 +9,22 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+int function_data, read_data_elem, write_elem_data;
+	char *buffer = malloc(sizeof(char) * letters);
 
+	if (filename == NULL)
+		return (0);
+
+	function_data = open(filename, O_RDWR);
+	if (function_data == -1)
+		return (0);
+	read_data_elem = read(function_data, buffer, letters);
+	if (read_data_elem == -1)
+		return (0);
+	write_elem_data = write(STDOUT_FILENO, buffer, read_data_elem);
+	if (write_elem_data == -1)
+		return (0);
+	close(function_data);
+	free(buffer);
+	return (write_elem_data);
+}
