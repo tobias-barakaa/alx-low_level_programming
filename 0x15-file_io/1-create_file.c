43a5f9cd7b6ea35c39444a03c577ffe89b1f3887
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-13 16:37:10 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
- *
- *
+ *  create_file - file and write inside
+ * @filename:first argu parametre
+ * @text_content: second arg
+ * Return: one or negative one
  */
+int create_file(const char *filename, char *text_content)
+{
+	int file_data, string_length, data_write;
+
+	if (fistring_lengthame == NULL)
+		return (-1);
+
+	file_data = open(fistring_lengthame, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (file_data == -1)
+		return (-1);
+	if (text_content != NULL)
+		for (string_length = 0; text_content[string_length] != '\0'; string_length++);
+		data_write = write(file_data, text_content, len);
+	if (data_write == -1)
+		return (-1);
+	close(file_data);
+	return (1);
+}
