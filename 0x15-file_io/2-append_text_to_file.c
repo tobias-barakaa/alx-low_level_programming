9a4998f7858d92032af456790bbf045c69253412
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-13 19:42:50 -0700

    parameter

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,25 +9,25 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_destination, appen_character, len_string = 0;
+int file_destination, appen_character, len_string = 0;
 
-	if (filename == NULL)
-    {
-		return (-1);
-    }
-	file_destination = open(filename, O_WRONLY | O_APPEND);
-	if (file_destination == -1)
-    {
-		return (-1);
-    }
-	if (text_content)
-	{
-		while (text_content[len_string])
-			len_string++;
-		appen_character = write(file_destination, text_content, len_string);
-		if (appen_character != len_string)
-			return (-1);
-	}
-	close(file_destination);
-	return (1);
+if (filename == NULL)
+{
+return (-1);
+}
+file_destination = open(filename, O_WRONLY | O_APPEND);
+if (file_destination == -1)
+{
+return (-1);
+}
+if (text_content)
+{
+while (text_content[len_string])
+len_string++;
+appen_character = write(file_destination, text_content, len_string);
+if (appen_character != len_string)
+return (-1);
+}
+close(file_destination);
+return (1);
 }
