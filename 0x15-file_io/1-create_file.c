b4b8ae1f205961fc23a3eae6d7aaa89a40e2aa18
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-13 20:01:17 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,37 +9,32 @@
 int create_file(const char *filename, char *text_content)
 {
 
-	int file_data_elem_data_name;
-	int count_iterate;
-	int read_only_write;
+int file_data_elem_data_name;
+int count_iterate;
+int read_only_write;
 
-	if (!filename)
-    {
-		return (-1);
-    }
+if (!filename)
+{
+return (-1);
+}
 
-	file_data_elem_data_name = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+file_data_elem_data_name = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (file_data_elem_data_name == -1)
-    {
-		return (-1);
-    }
+if (file_data_elem_data_name == -1)
+{
+return (-1);
+}
 
-	if (!text_content)
-    {
-		text_content = "";
-    }
+if (!text_content)
+{
+text_content = "";
+}
+for (count_iterate = 0; text_content[count_iterate]; count_iterate++)
+read_only_write = write(file_data_elem_data_name, text_content, count_iterate);
 
-	for (count_iterate = 0; text_content[count_iterate]; count_iterate++);
+if (read_only_write == -1)
+return (-1);
+close(file_data_elem_data_name);
 
-	read_only_write = write(file_data_elem_data_name, text_content, count_iterate);
-
-	if (read_only_write == -1)
-		return (-1);
-
-	close(file_data_elem_data_name);
-
-	return (1);
-
-
+return (1);
 }
