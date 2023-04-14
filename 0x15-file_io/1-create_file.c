9a75d5c4acb330c0989938e5b78436754d2657e5
tobias-barakaa <tobiasbarakan@gmail.com>
2023-04-13 19:53:00 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,19 +8,38 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_data, string_leng, data_write;
 
-	if (filename == NULL)
+	int file_data_elem_data_name;
+	int count_iterate;
+	int read_only_write;
+
+	if (!filename)
+    {
+		return (-1);
+    }
+
+	file_data_elem_data_name = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file_data_elem_data_name == -1)
+    {
+		return (-1);
+    }
+
+	if (!text_content)
+    {
+		text_content = "";
+    }
+
+	for (count_iterate = 0; text_content[count_iterate]; count_iterate++);
+
+	read_only_write = write(file_data_elem_data_name, text_content, count_iterate);
+
+	if (read_only_write == -1)
 		return (-1);
 
-	file_data = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
-	if (file_data == -1)
-		return (-1);
-	if (text_content != NULL)
-	for (string_leng = 0; text_content[string_leng] != '\0'; string_leng++)
-		data_write = write(file_data, text_content, string_leng);
-	if (data_write == -1)
-		return (-1);
-	close(file_data);
+	close(file_data_elem_data_name);
+
 	return (1);
+
+
 }
