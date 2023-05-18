6d7df0081193ee5cdd67105b3573eaecf4100a47
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-18 11:00:31 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,20 +4,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - doubly linked list print
+ *print_dlistint - doubly linked list print
  * @h:argument
+ * Return: counts
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	int num = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		num++;
 	}
-	
-	return(num);
+	return (num);
 }
