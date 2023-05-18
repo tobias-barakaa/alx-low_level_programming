6299a75f95a71239fa98ec586a83f3eeda9ae787
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-18 11:55:34 -0700

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,11 +14,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	struct dlistint_s  *ptr = (struct dlistint_s *)malloc(sizeof(struct dlistint_s));  
 
 	 if(ptr == NULL)
-		 return NULL;
 	 {
+		 return NULL;
+	 }
 		 ptr->n = n;
 		 ptr->next = NULL;
-			 ptr->prev = NULL;
-			 return (ptr);
-	 }
+		 ptr->prev = NULL;
+    if (*head != NULL)
+    {
+        (*head)->prev = ptr;
+    }
+
+    *head = ptr;
+
+    return ptr;
 }
