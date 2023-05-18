97ed46bd9e832e5ee43205745fdaa89e6cc8f4a1
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-18 11:22:22 -0700

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,16 +6,17 @@
 /**
  * dlistint_len - lenth elements
  * @h: arguments
+ * Return: elem num
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	int num = 0;
-	
-	while (h != NULL)
-	{
-		num++;
-		h = h->next;
-	}
-	return (num);
+int num = 0;
+
+while (h != NULL)
+{
+num++;
+h = h->next;
+}
+return (num);
 }
