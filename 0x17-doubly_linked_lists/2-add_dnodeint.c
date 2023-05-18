61fdd473045974698911879aea2d5b904df25a48
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-18 12:01:52 -0700

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,15 +17,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	 {
 		 return NULL;
 	 }
-		 ptr->n = n;
 		 ptr->next = NULL;
 		 ptr->prev = NULL;
+		 ptr->n = n;
     if (*head != NULL)
     {
         (*head)->prev = ptr;
+	(*head)->next = ptr;
     }
-
-    *head = ptr;
-
     return ptr;
 }
