3c3e8dfda698531b128fe07192a7f6fe10f38d82
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-18 13:02:43 -0700

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,16 +7,18 @@
  * add_dnodeint - function to add beginn
  * @head: first argument head
  * @n: second argument
+ * Return: ptr
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-struct dlistint_s  *ptr = (struct dlistint_s *)malloc(sizeof(struct dlistint_s));  
+struct dlistint_s
+*ptr = (struct dlistint_s *)malloc(sizeof(struct dlistint_s));
 
-if(ptr == NULL)
+if (ptr == NULL)
 {
-return NULL;
-}	 
+return (NULL);
+}
 ptr->n = n;
 ptr->prev = NULL;
 ptr->next = *head;
@@ -25,5 +27,5 @@ if (*head != NULL)
 (*head)->prev = ptr;
 }
 *head = ptr;
-return ptr;
+return (ptr);
 }
