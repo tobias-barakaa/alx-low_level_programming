458b730073cbaf5bb4e8bc9f7d7ae2526a4fab4e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-18 17:10:23 -0700

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,30 +9,26 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *curr = *head;
-	dlistint_t *next_curr;
+dlistint_t *curr = *head;
+dlistint_t *next_curr;
+next_curr = malloc(sizeof(dlistint_t));
+if (next_curr == NULL)
+return (NULL);
+next_curr->n = n;
+next_curr->next = NULL;
+if (curr != NULL)
+{
+while (curr->next != NULL)
+{
+curr = curr->next;
+}
+curr->next = next_curr;
+}
+else
+{
+*head = next_curr;
+}
+next_curr->prev = curr;
 
-	next_curr = malloc(sizeof(dlistint_t));
-	if (next_curr == NULL)
-		return (NULL);
-
-	next_curr->n = n;
-	next_curr->next = NULL;
-
-	if (curr != NULL)
-	{
-		while (curr->next != NULL)
-        {
-			curr = curr->next;
-        }
-		curr->next = next_curr;
-	}
-	else
-	{
-		*head = next_curr;
-	}
-
-	next_curr->prev = curr;
-
-	return (next_curr);
+return (next_curr);
 }
