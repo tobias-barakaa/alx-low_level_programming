30a92b4a8be11c4817d7eeba2a8abd97b96c50b2
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-22 07:53:22 -0700

    0x18

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * add - function to add
+ * @a: first arg
+ * @b: second
+ * Return: a,b
+ */
+
+int add(int a, int b) {
+    return a + b;
+}
+
+/**
+ * sub - function to sub
+ * @a: first arg
+ * @b: second a
+ * Return: a,b
+ */
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+/**
+ * mul - function to multiply
+ * @a: first arg
+ * @b: second a
+ */
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+/**
+ * div - function to divide
+ * @a: first arg
+ * @b: second a
+ * Return:a,b
+ */
+int div(int a, int b) {
+    return a / b;
+}
+
+/**
+ * mod - function to modulus
+ * @a: first arg
+ * @b: secondar
+ * Return:a,b
+ */
+int mod(int a, int b) {
+    return a % b;
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -21,5 +21,12 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+int div(int a, int b);
+
+
 
 #endif
