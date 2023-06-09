fcc1fc67394ed5580d4e8ea5a4944082c206576b
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-09 15:26:45 -0700

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-unsigned long int hash_value = hash_djb2(key);
-unsigned long int index = hash_value % size;
-return (index);
+return(hash_djb2(key) % size);
 }
