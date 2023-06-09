bf19bbf8cc02c410344c984aaae0876805422834
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-09 12:59:37 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -37,3 +37,9 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 } hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+
+
+#endif /* __hash_tables__ */
