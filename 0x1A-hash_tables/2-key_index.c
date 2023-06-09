bdf3431fd28155431f00cea4ed0cde6774353cdb
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-09 15:48:08 -0700

    hash_tables.h

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index = function to find index
+ * @size: size
+ * Return: retur value
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+return (hash_djb2(key) % size);
+}
