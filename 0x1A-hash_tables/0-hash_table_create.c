0d77c6586f6a07366c36a5c9f67e62c7575716c2
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-09 13:11:03 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,32 +2,26 @@
 
 /**
  * hash_table_create - hash-table function
- * @size - size 
- * return: return the hash-value
+ * @size: size
+ * Return: return the hash-value
+ *
 */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    unsigned long int i;
-    
-    
-    hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
-    return (NULL);
-        
-       
-    hash_table->size = size;
-    hash_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (hash_table->array == NULL)
-    {
-        return (NULL);
-        }
-    
-    
-    for (i = 0; i < size; i++)
-    
-    hash_table->array[i] = NULL;
-        hash_table->size = size;
-        
-        return (hash_table);
+	unsigned long int i;
+
+hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+if (hash_table == NULL)
+return (NULL);
+hash_table->size = size;
+hash_table->array = malloc(sizeof(hash_node_t *) * size);
+if (hash_table->array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+hash_table->array[i] = NULL;
+hash_table->size = size;
+return (hash_table);
 }
