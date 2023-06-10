c39c993be9f4dec7667f82b1b7cc58cdc730e930
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-09 17:07:20 -0700

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,28 +9,28 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
-	hash_node_t *node;
+unsigned long int index;
+hash_node_t *node;
 
-	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
-		return (NULL);
+if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+return (NULL);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
-	if (node == NULL)
-    {
-		return (NULL);
-    }
-	while (strcmp(node->key, key) && node != NULL)
-	{
-		node = node->next;
-	}
-	if (node == NULL)
-    {
-		return (NULL);
-    }
-	else
-    {
-		return (node->value);
-    }
+index = key_index((const unsigned char *)key, ht->size);
+node = ht->array[index];
+if (node == NULL)
+{
+return (NULL);
+}
+while (strcmp(node->key, key) && node != NULL)
+{
+node = node->next;
+}
+if (node == NULL)
+{
+return (NULL);
+}
+else
+{
+return (node->value);
+}
 }
