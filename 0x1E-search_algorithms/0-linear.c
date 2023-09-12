8c75cc48ebb46d7a736ec1dae619c762ae57065c
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-12 18:25:37 +0300

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,12 +6,13 @@
 *@value: data to check
 *@size: length of the arr
 *
-*Return: Always EXIT_SUCCESS
+*Return: EXIT_SUCCESS
 */
 
 int linear_search(int *array, size_t size, int value)
 {
 size_t i;
+  
 if (!array)
 return (-1);
 for (i = 0; i < size; i++)
