b1efbaf7945772e32a737c15586f967bc03a6dc6
tobias-barakaa <tobiasbarakan@gmail.com>
2023-09-12 12:39:54 -0700

    1-binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,7 +11,7 @@
 int binary_search(int *array, size_t size, int value)
 {
 size_t i, left = 0, right = size - 1, mid;
-while (left < right)
+while (left <= right)
 {
 printf("Searching in array: ");
 for (i = left; i <= right; i++)
