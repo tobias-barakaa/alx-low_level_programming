9df5e61b92d7ab6ac3b472e8d96818c5fc1ef935
tobias-barakaa <tobiasbarakan@gmail.com>
2023-09-12 07:50:46 -0700

    linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+*linear_search - linear search function
+*@array: list of num or arrays
+*@value: data to check
+*@size: length of the arr
+*
+*Return: value otherwise
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+size_t i;
+if (array == NULL)
+return (-1);
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+}
+return (-1);
+}
