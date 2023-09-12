aafc4446331e0ac9aae9ef9f8ce0cdecfb1f4858
tobias-barakaa <tobiasbarakan@gmail.com>
2023-09-12 05:42:50 -0700

    0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,17 +6,22 @@
 *linear_search - linear search function
 *@array: list of num or arrays
 *@value: data to check
-**/
+*@size: length of the arr
+*
+*Return: value otherwise
+*/
 
 int linear_search(int *array, size_t size, int value)
 {
-if (array == NULL)
-return (-1); // Exit with an error code
-for(size_t i = 0; i < size; i++)
+if (array == NULL || size <= 0)
+return (-1);
+for (size_t i = 0; i < size; i++)
 {
 printf("Value checked array[%zu] = [%d]\n", i, array[i]);
 if (array[i] == value)
-printf("Found %d at index: %zu\n", array[i], i);
+{
+return (i);
+}
 }
 return (-1);
 }
