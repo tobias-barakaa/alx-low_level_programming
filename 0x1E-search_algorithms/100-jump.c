7ab41f4c7a24a244e6382a6162db22aad8328557
tobias-barakaa <tobiasbarakan@gmail.com>
2023-09-13 15:43:22 -0700

    search_algos

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,6 +14,8 @@ int jump_search(int *array, size_t size, int value)
     size_t step,i,left = 0;
 
     step = sqrt(size);
+    if(array == NULL)
+    return (-1);
 
     while (left < size && array[left] < value)
     {
